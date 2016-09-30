347c1f5177930a493b25be3e4ea43dfc00b66067
HDDinnova Web Services <info@hddinnova.com>
2016-09-30 14:11:58 +0200

    modificacions a registre

--- src/api/index.php
+++ src/api/index.php
@@ -16,36 +16,62 @@ Flight::route('/', function(){
 Flight::route('/newCompetitor', function(){
     $db = Flight::db();
 
+    function randomPassword() {
+        $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+        $pass = array();
+        $alphaLength = strlen($alphabet) - 1;
+        for ($i = 0; $i < 8; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        return implode($pass);
+    }
+
     $dades = file_get_contents('php://input');
     $dades = mb_convert_encoding($dades, 'HTML-ENTITIES', "UTF-8");
 
     $post = json_decode($dades,true);
-    $sql = "INSERT INTO competitors(section, nfilms, fullName, comName, vat, address, zip, city, country, phone, email, date, website, facebook) VALUES (:section, :nfilms, :fullName, :comName, :vat, :address, :zip, :city, :country, :phone, :email, :date, :website, :facebook)";
 
-    $new = $db->prepare($sql);
-    $new->bindParam(':section', $post['section']);
-    $new->bindParam(':nfilms', $post['nfilms'], PDO::PARAM_INT);
-    $new->bindParam(':fullName', $post['pname']);
-    $new->bindParam(':comName', $post['cname']);
-    $new->bindParam(':vat', $post['vat']);
-    $new->bindParam(':address', $post['address']);
-    $new->bindParam(':zip', $post['zip']);
-    $new->bindParam(':city', $post['city']);
-    $new->bindParam(':country', $post['country']);
-    $new->bindParam(':phone', $post['phone']);
-    $new->bindParam(':email', $post['email']);
-    $new->bindParam(':date', $post['date']);
-    $new->bindParam(':website', $post['web']);
-    $new->bindParam(':facebook', $post['facebook']);
+    // Primer comprovem que l'usuari no estigui registrat
+    $sql = "SELECT * FROM competitors WHERE email = :email LIMIT 1";
+    $check = $db->prepare($sql);
+    $check->bindParam(':email', $post['email']);
+    $check->execute();
+    $count = $check->rowCount();
 
-    try {
-      if ($new->execute()) {
-        echo true;
-      } else {
-        echo false;
+    if ($count === 0){
+      $sql = "INSERT INTO competitors(section, nfilms, fullName, comName, vat, address, zip, city, country, phone, email, date, website, facebook, password) VALUES (:section, :nfilms, :fullName, :comName, :vat, :address, :zip, :city, :country, :phone, :email, :date, :website, :facebook, :password)";
+
+      $newpass = randomPassword();
+
+      $new = $db->prepare($sql);
+      $new->bindParam(':section', $post['section']);
+      $new->bindParam(':nfilms', $post['nfilms'], PDO::PARAM_INT);
+      $new->bindParam(':fullName', $post['pname']);
+      $new->bindParam(':comName', $post['cname']);
+      $new->bindParam(':vat', $post['vat']);
+      $new->bindParam(':address', $post['address']);
+      $new->bindParam(':zip', $post['zip']);
+      $new->bindParam(':city', $post['city']);
+      $new->bindParam(':country', $post['country']);
+      $new->bindParam(':phone', $post['phone']);
+      $new->bindParam(':email', $post['email']);
+      $new->bindParam(':date', $post['date']);
+      $new->bindParam(':website', $post['web']);
+      $new->bindParam(':facebook', $post['facebook']);
+      $new->bindParam(':password', $newpass);
+
+      try {
+        if ($new->execute()) {
+          echo true;
+        } else {
+          echo false;
+        }
+      } catch (Exception $e) {
+        echo 'Exception: ',  $e->getMessage();
       }
-    } catch (Exception $e) {
-      echo 'Exception: ',  $e->getMessage();
+    } else {
+      echo 'exist';
     }
 
     $db = NULL;
